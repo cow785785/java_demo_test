f17c02df37f06e6442054310d1a37b4c0e4b3c96
cow785785 <zcbm030199@gmail.com>
2023-03-20 09:20:37 +0800

    delete test

--- src/main/java/com/example/java_demo_test/JavaDemoTestApplication_0317.java
+++ src/main/java/com/example/java_demo_test/JavaDemoTestApplication_0317.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaDemoTestApplication {
+public class JavaDemoTestApplication_0317 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaDemoTestApplication.class, args);
