6cf43e6c1bbd6a9345ba3581dc1d1d2013826ba6
cow785785 <zcbm030199@gmail.com>
2023-03-20 09:18:21 +0800

    0320

--- src/main/java/com/example/java_demo_test/JavaDemoTestApplication_0317_2.java
+++ src/main/java/com/example/java_demo_test/JavaDemoTestApplication_0317_2.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaDemoTestApplication {
+public class JavaDemoTestApplication_0317_2 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaDemoTestApplication.class, args);
